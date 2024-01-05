d36edece554febc2364f7f23bab9eddbf15276dc
Sherif Hamdy <shamdy@xapps.co>
2024-01-05 16:37:27 +0200

    Initial Commit

---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Sherif Hamdy on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
